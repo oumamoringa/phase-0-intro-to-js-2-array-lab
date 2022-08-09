b4105c9b0cc11af9768e9b7e3e26acd806f557ca
oumamoringa <richard.ouma@student.moringaschool>
2022-08-09 14:06:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const   cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");   
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+
+function appendCat(name){
+    const newcats = cats.slice();
+    newcats.push("Broom");
+    return newcats;
+}
+
+function prependCat(name){
+    const newarray = cats.slice();
+    newarray.unshift("Arnold");
+    return newarray;
+}
+
+function removeLastCat(){
+    const last = cats.slice();
+    last.pop()
+    return last;
+}
+
+function removeFirstCat(){
+    const rem = cats.slice();
+    rem.shift();
+    return rem;
+}
